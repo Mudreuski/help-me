09807a530a7d747a4d497989f5165cb43494ef93
Mudreuski <bahus1994@mail.ru>
2018-10-07 22:49:58 +0300

    solution 11/17

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 module.exports = function count(s, pairs) {
-  // your implementation
+  let N = 1;
+  let cache = 0;
+  let step = [];
+  let step2 = [];
+  let result = 0;
+
+  for (let i = 0, len = pairs.length; i < len; i += 1) {
+    N *= pairs[i][0] ** pairs[i][1];
+  }
+  if (N > 9999999) {
+    return 0;
+  }
+
+  for (let j = 0, len = s.length; j < len; j += 1) {
+    if (s[j] === '0' && j === 0) {
+      for (let k = 0; k < N; k += 1) {
+        cache = k + j;
+
+        if (nod(N, cache) !== 1) {
+          step.push(k);
+        }
+      }
+    } else if (s[j] === '1' && j === 0) {
+      for (let k = 0; k < N; k += 1) {
+        cache = k + j;
+
+        if (nod(N, cache) === 1) {
+          step.push(k);
+        }
+      }
+    } else if (s[j] === '0') {
+      for (let k = 0; k <= N; k += 1) {
+        cache = k + j;
+
+        if (nod(N, cache) !== 1 && step.includes(k)) {
+          step2.push(k);
+        }
+      }
+
+      step = step2;
+      step2 = [];
+    } else if (s[j] === '1') {
+      for (let k = 0; k <= N; k += 1) {
+        cache = k + j;
+
+        if (nod(N, cache) === 1 && step.includes(k)) {
+          step2.push(k);
+        }
+      }
+
+      step = step2;
+      step2 = [];
+    }
+  }
+
+  result = step.length % 10000000007;
+  return result;
+};
+
+function nod(a, b) {
+  if (b === 0) return a;
+  return nod(b, a % b);
 }
